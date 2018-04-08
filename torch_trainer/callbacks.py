c9514510c1d7e93902791df9f12b9c7cb1106682
Christopher Moody <chrisemoody@gmail.com>
2018-04-08 11:36:17 -0700

    Initial commit

--- torch_trainer/callbacks.py
+++ torch_trainer/callbacks.py
@@ -1,10 +1,19 @@
 from sklearn.metrics import roc_auc_score
+# from sklearn.metrics import mean_squared_error
 import numpy as np
 
 
 def auc_callback(batch, model, pred):
-    target = batch[-1].data.numpy()
-    pred = pred.data.numpy()
+    target = batch[-1].data.cpu().numpy()
+    pred = pred.data.cpu().numpy()
     idx = np.isfinite(pred)
     pred[~idx] = 0.0
     return roc_auc_score(target, pred)
+
+
+def rms_callback(batch, model, pred):
+    target = batch[-1].data.cpu().numpy()
+    pred = pred.data.cpu().numpy()
+    idx = np.isfinite(pred)
+    pred[~idx] = 0.0
+    return np.sqrt(((target - pred)**2.0).mean())
--- torch_trainer/trainer.py
+++ torch_trainer/trainer.py
@@ -1,4 +1,5 @@
 import time
+import random
 import pandas as pd
 
 import torch
@@ -6,7 +7,28 @@ from torch.autograd import Variable
 from sklearn.utils import shuffle
 
 
-def chunks(batchsize, *arrs):
+def to_32(arr):
+    if 'int' in str(arr.dtype):
+        return arr.astype('int64')
+    else:
+        return arr.astype('float32')
+
+
+def chunks(batchsize, *arrs, cuda=False):
+    n = batchsize
+    lens = [arr.shape[0] for arr in arrs]
+    err = "Not all arrays are of same shape"
+    length = lens[0]
+    assert all(length == l for l in lens), err
+    for i in range(0, length, n):
+        var = [Variable(torch.from_numpy(to_32(arr[i:i + n])))
+               for arr in arrs]
+        if cuda:
+            var = [v.cuda() for v in var]
+        yield var
+
+
+def chunk_shuffle(batchsize, *arrs):
     n = batchsize
     lens = [arr.shape[0] for arr in arrs]
     err = "Not all arrays are of same shape"
@@ -17,7 +39,7 @@ def chunks(batchsize, *arrs):
 
 
 class Trainer(object):
-    def __init__(self, model, optimizer, callbacks={}, seed=42,
+    def __init__(self, model, optimizer, callbacks={}, seed=42, cuda=False,
                  print_every=25, batchsize=2048, window=500, clip=None):
         self.model = model
         self.optimizer = optimizer
@@ -31,24 +53,61 @@ class Trainer(object):
         self.batchsize = batchsize
         self.window = window
         self.clip = clip
+        self.cuda = cuda
 
     def fit(self, *args):
         # args is X1, X2,...Xn, Yn
+        self.model.train(True)
         self._iteration = 0
-        args = shuffle(*args, random_state=self.seed + self._epoch)
-        for batch in chunks(self.batchsize, *args):
+        rs = random.randint(0, 100000)
+        args = shuffle(*args, random_state=rs)
+        # , random_state=self.seed + self._epoch)
+        for batch in chunks(self.batchsize, *args, cuda=self.cuda):
             start = time.time()
-            target = batch[-1]
             self.optimizer.zero_grad()
-            pred = self.model.forward(*batch[:-1])
-            loss = self.model.loss(pred, target)
-            loss.backward()
+            pred = self.model.forward(*batch)
+            loss = self.model.loss(pred, *batch)
+            scalar = sum(loss)
+            scalar.backward()
+            grad_norm = max(p.grad.data.abs().max()
+                            for p in self.model.parameters()
+                            if p.grad is not None)
             if self.clip:
                 torch.nn.utils.clip_grad_norm(self.model.parameters(),
                                               self.clip)
             self.optimizer.step()
             stop = time.time()
-            self.run_callbacks(batch, pred, loss=loss.data[0],
+            kwargs = {f'loss_{i}': l.data[0] for i, l in enumerate(loss)}
+            kwargs['grad_norm_max'] = grad_norm
+            self.run_callbacks(batch, pred, train=True, iter_time=stop-start,
+                               **kwargs)
+            if self._iteration % self.print_every == 0:
+                self.print_log(header=self._iteration == 0)
+            self._iteration += 1
+        self._epoch += 1
+        self.previous_log.extend(self.log)
+        self.log = []
+        self.model.train(False)
+
+    def fit_sequence(self, inputs, labels, lengths):
+        args = (inputs, labels, lengths)
+        for input, label, length in chunk_shuffle(self.batchsize, *args):
+            start = time.time()
+            for frame in range(length.max()):
+                self.optimizer.zero_grad()
+                input_frame = input[:, frame]
+                label_frame = label[:, frame]
+                pred_frame = self.model.forward(input_frame)
+                loss = self.model.loss(pred_frame, label_frame)
+                loss.backward()
+                if self.clip:
+                    torch.nn.utils.clip_grad_norm(self.model.parameters(),
+                                                  self.clip)
+                self.optimizer.step()
+            stop = time.time()
+            # We estimate AUC just on the last item
+            self.run_callbacks([input_frame, label_frame], pred_frame,
+                               loss=loss.data[0],
                                train=True, iter_time=stop-start)
             if self._iteration % self.print_every == 0:
                 self.print_log(header=self._iteration == 0)
@@ -60,16 +119,24 @@ class Trainer(object):
     def test(self, *args):
         # args is X1, X2...Xn, Y
         # Where Xs are features, Y is the outcome
+        self.model.train(False)
         self._iteration = 0
-        for batch in chunks(self.batchsize, *args):
+        self.optimizer.zero_grad()
+        for batch in chunks(self.batchsize, *args, cuda=self.cuda):
             target = batch[-1]
             pred = self.model.forward(*batch[:-1])
             loss = self.model.loss(pred, target)
-            self.run_callbacks(batch, pred, train=False, loss=loss.data[0])
+            scalar = sum(loss)
+            scalar.backward()
+            self.run_callbacks(batch, pred, train=False, loss=scalar.data[0])
             if self._iteration % self.print_every == 0:
                 self.print_log(header=self._iteration == 0)
             self._iteration += 1
+        self.optimizer.zero_grad()
         self._iteration = 0
+        self.previous_log.extend(self.log)
+        self.log = []
+        self.model.train(True)
 
     def run_callbacks(self, batch, pred, **kwargs):
         vals = {name: cb(batch, self.model, pred)
@@ -93,7 +160,7 @@ class Trainer(object):
             if key in concat.columns:
                 del concat[key]
         line = (concat.tail(1)
-                      .applymap("{0:1.4f}".format)
+                      .applymap("{0:1.2e}".format)
                       .to_string(header=header))
         print(line)
 
